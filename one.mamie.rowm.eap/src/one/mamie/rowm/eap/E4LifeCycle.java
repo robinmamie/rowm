d0646ab03b99bd1b2cc3d5d2bd9807e5000c34e6
Robin Mamié <robin@mamie.one>
2023-01-19 20:32:46 +0100

    Fix style problems

--- one.mamie.rowm.eap/src/one/mamie/rowm/eap/E4LifeCycle.java
+++ one.mamie.rowm.eap/src/one/mamie/rowm/eap/E4LifeCycle.java
@@ -7,7 +7,8 @@ import org.eclipse.e4.ui.workbench.lifecycle.ProcessAdditions;
 import org.eclipse.e4.ui.workbench.lifecycle.ProcessRemovals;
 
 /**
- * This is a stub implementation containing e4 LifeCycle annotated methods.<br />
+ * This is a stub implementation containing e4 LifeCycle annotated
+ * methods.<br />
  * There is a corresponding entry in <em>plugin.xml</em> (under the
  * <em>org.eclipse.core.runtime.products' extension point</em>) that references
  * this class.
@@ -16,17 +17,21 @@ public class E4LifeCycle {
 
 	@PostContextCreate
 	void postContextCreate(IEclipseContext workbenchContext) {
+		// Boilerplate
 	}
 
 	@PreSave
 	void preSave(IEclipseContext workbenchContext) {
+		// Boilerplate
 	}
 
 	@ProcessAdditions
 	void processAdditions(IEclipseContext workbenchContext) {
+		// Boilerplate
 	}
 
 	@ProcessRemovals
 	void processRemovals(IEclipseContext workbenchContext) {
+		// Boilerplate
 	}
 }
